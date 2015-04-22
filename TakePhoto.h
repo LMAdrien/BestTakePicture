999f98f82d4bb8ad6681d0efe3fc4c68d3515aae
LE MOIGNE Adrien <lemoig_a@etna-alternance.net>
2015-04-22 10:15:37 +0200

    Begin app empty

---
+++ TakePhoto.h
@@ -0,0 +1,13 @@
+//
+//  TakePhoto.h
+//  Localeyes
+//
+//  Created by Adrien LE MOIGNE on 21/04/2015.
+//  Copyright (c) 2015 lemoigne.adrien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TakePhoto : NSObject
+
+@end
---
+++ TakePhoto.m
@@ -0,0 +1,13 @@
+//
+//  TakePhoto.m
+//  Localeyes
+//
+//  Created by Adrien LE MOIGNE on 21/04/2015.
+//  Copyright (c) 2015 lemoigne.adrien. All rights reserved.
+//
+
+#import "TakePhoto.h"
+
+@implementation TakePhoto
+
+@end
