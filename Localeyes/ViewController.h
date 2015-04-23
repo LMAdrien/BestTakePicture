62ef8ed5685b891ef8eb4822a065aeaa8d1c9774
LE MOIGNE Adrien <lemoig_a@etna-alternance.net>
2015-04-23 11:55:41 +0200

    App Run OK
    Load and change IMG static
    Record img to array => NSMUTABLEARRAY object

--- Localeyes/ViewController.h
+++ Localeyes/ViewController.h
@@ -8,8 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+// View NS LOG DEBUG
+static BOOL ERRORNSLOG = true;
+
 @interface ViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
+@property (weak, nonatomic) IBOutlet UIImageView *ViewPicture;
+@property (weak, nonatomic) IBOutlet UIImageView *ViewCamera;
+@property(nonatomic, retain) UIImage *image;
+@property(nonatomic, retain) UIImage *outImage;
+@property(nonatomic, retain) NSMutableArray *ProspectImage;
+
+- (IBAction)ActionShot:(id)sender;
 
 @end
 
--- Localeyes/ViewController.m
+++ Localeyes/ViewController.m
@@ -14,15 +14,35 @@
 
 @implementation ViewController
 
+
+/*
+ *  Initialize object
+ */
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
+    _image = [[UIImage alloc]init];
+    _outImage = [[UIImage alloc] init];
+    _ProspectImage = [[NSMutableArray alloc] init];
+    _image = [UIImage imageNamed:@"etna"];
+    _ViewPicture.image = _image;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+/*
+ *  Action button shot camera
+ */
+- (IBAction)ActionShot:(id)sender {
+    NSInteger NbElementsArray = _ProspectImage.count;
+    _ViewCamera.image = _ViewPicture.image;
+   [_ProspectImage insertObject:_ViewPicture.image atIndex:NbElementsArray];
+    if(ERRORNSLOG == true)
+    {
+        for (NSInteger i = 0; i < _ProspectImage.count; i++) {
+            NSLog(@"%@", [_ProspectImage[i] description]);
+        }
+    }
+}
 @end
