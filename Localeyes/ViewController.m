a20bf0291d7a10076f2af7f77994367c91189e6b
Adrien LE MOIGNE <lemoig_a@sm301-r4p09.local>
2015-04-23 19:52:42 +0200

    Live streaming OK

--- Localeyes/ViewController.m
+++ Localeyes/ViewController.m
@@ -7,24 +7,25 @@
 //
 
 #import "ViewController.h"
-
+#import <AVFoundation/AVFoundation.h>
 @interface ViewController ()
 
 @end
 
-@implementation ViewController 
+@implementation ViewController
 
-
+    AVCaptureSession *session;
+    AVCaptureStillImageOutput *stillImageOutput;
 
 // Initialize object
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _image = [[UIImage alloc]init];
-    _outImage = [[UIImage alloc] init];
-    _ProspectImage = [[NSMutableArray alloc] init];
-    _image = [UIImage imageNamed:@"etna"];
-    _ViewPicture.image = _image;
+    //_image = [[UIImage alloc]init];
+    //_outImage = [[UIImage alloc] init];
+    //_ProspectImage = [[NSMutableArray alloc] init];
+    //_image = [UIImage imageNamed:@"etna"];
+    //_ViewPicture.image = _image;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,12 +33,11 @@
     }
 
 //  Action button shot camera
-
+/*
 - (IBAction)ActionShot:(id)sender {
     NSInteger NbElementsArray = _ProspectImage.count;
     _ViewCamera.image = _ViewPicture.image;
    [_ProspectImage insertObject:_ViewPicture.image atIndex:NbElementsArray];
-    [self takePhoto];
     [_NumberShoot setText:[NSString stringWithFormat: @"%li",_ProspectImage.count]];
     if(ERRORNSLOG == true)
     {
@@ -48,13 +48,60 @@
 }
 //  Action button to next column in department store
 - (IBAction)ActionNextColumn:(id)sender {
-    [self takePhoto];
+    [self getCamera];
 }
-
+*/
 
 /*
  * TESTING BEGIN
  */
+
+-(void)viewWillAppear:(BOOL)animated{
+    session = [[AVCaptureSession alloc] init];
+    [session setSessionPreset:AVCaptureSessionPresetPhoto];
+    AVCaptureDevice *inputDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error;
+    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:inputDevice error:&error];
+    if ([session canAddInput:deviceInput])
+        [session addInput:deviceInput];
+    AVCaptureVideoPreviewLayer *previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
+    [previewLayer setVideoGravity:AVLayerVideoGravityResizeAspect];
+    CALayer *rootLayer = [[self view] layer];
+    [rootLayer setMasksToBounds:YES];
+    CGRect frame = self.FrameForCapture.frame;
+    [previewLayer setFrame:frame];
+    [rootLayer insertSublayer:previewLayer atIndex:0];
+    stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys:AVVideoCodecJPEG, AVVideoCodecKey, nil];
+    [stillImageOutput setOutputSettings:outputSettings];
+    [session addOutput:stillImageOutput];
+    [session startRunning];
+    
+}
+
+- (IBAction)takePhoto:(id)sender {
+    
+    AVCaptureConnection *videoConnection = nil;
+    for (AVCaptureConnection *connection in stillImageOutput.connections) {
+        for (AVCaptureInputPort *port in [connection inputPorts]) {
+            if ([[port mediaType] isEqual:AVMediaTypeVideo]){
+                videoConnection = connection;
+                break;
+            }
+        }
+        if (videoConnection) {
+            break;
+        }
+    }
+    [stillImageOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+        if (imageDataSampleBuffer != NULL) {
+            NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+            UIImage *image = [UIImage imageWithData:imageData];
+            self.imageView.image = image;
+        }
+    }];
+}
+/*
 - (IBAction)selectPhoto:(UIButton *)sender {
     
     UIImagePickerController *picker = [[UIImagePickerController alloc] init];
@@ -65,7 +112,7 @@
     [self presentViewController:picker animated:YES completion:NULL];
 }
 
--(void)takePhoto{
+-(void)takePhotoOld{
     UIImagePickerController *picker = [[UIImagePickerController alloc] init];
     picker.delegate = self;
     picker.allowsEditing = YES;
@@ -94,7 +141,9 @@
     [self presentModalViewController:picker animated:YES];
     
 }
+ */
 /*
  * TESTING END
  */
+
 @end
